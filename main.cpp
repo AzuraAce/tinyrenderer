b5cb1ec8b55622d289f72790193b9c0409ece5ad
AzuraAce <oliverjbmub@gmail.com>
2025-09-13 17:26:42 +0200

    draw vertices and faces from .obj files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include "tgaimage.h"
+
+constexpr int width  = 800;
+constexpr int height = 800;
 
 constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
 constexpr TGAColor green   = {  0, 255,   0, 255};
@@ -39,23 +46,92 @@ line (int x1, int y1, int x2, int y2, TGAImage &framebuffer, TGAColor color)
     }
 }
 
+void
+line_3d () {}
+
+typedef struct vec3d {
+    float x;
+    float y;
+    float z;
+} Vec3d;
+
+typedef struct face {
+    int index1;
+    int index2;
+    int index3;
+} Face;
+
+void
+get_vertices_and_faces (std::fstream &FILE,
+                        std::vector<Vec3d> &vertices,
+                        std::vector<Face> &faces)
+{
+    std::string line;
+    while (std::getline (FILE,line))
+    {
+        if (line.size() > 2 && line[0] == 'v'
+            && std::isspace(line[1]))
+        {
+            float x, y, z;
+
+            // skip 'v' then three floats
+            if (std::sscanf(line.c_str() + 1,
+                            "%f %f %f", &x, &y, &z) == 3)
+            {
+                vertices.push_back(Vec3d{ x, y, z });
+            }
+        }
+        else if (line.size() > 2 && line[0] == 'f'
+                 && std::isspace(line[1]))
+        {
+            int index1, index2, index3;
+
+            if (std::sscanf(line.c_str() + 1,
+                            "%d/%*d/%*d %d/%*d/%*d %d/%*d/%*d", &index1, &index2, &index3) == 3)
+            {
+                faces.push_back(Face{index1,index2,index3});
+            }
+        }
+    }
+}
+
+std::tuple<int, int>
+project (Vec3d v)
+{
+    return {
+            (v.x + 1.f) * width / 2,
+            (v.y + 1.f) * height / 2
+           };
+}
+
 int main(int argc, char** argv) {
-    constexpr int width  = 64;
-    constexpr int height = 64;
     TGAImage framebuffer(width, height, TGAImage::RGB);
 
-    int ax =  7, ay =  3;
-    int bx = 12, by = 37;
-    int cx = 62, cy = 53;
+    std::vector<Vec3d> vertices;
+    std::vector<Face> faces;
+    std::fstream obj_file;
 
-    line(ax, ay, bx, by, framebuffer, blue);
-    line(cx, cy, bx, by, framebuffer, green);
-    line(cx, cy, ax, ay, framebuffer, yellow);
-    line(ax, ay, cx, cy, framebuffer, red);
+    obj_file.open("obj/diablo3_pose/diablo3_pose.obj", std::ios::in |
+                  std::ios::out);
 
-    framebuffer.set(ax, ay, white);
-    framebuffer.set(bx, by, white);
-    framebuffer.set(cx, cy, white);
+    if (obj_file.is_open())
+    {
+        get_vertices_and_faces(obj_file, vertices, faces);
+        obj_file.close();
+        size_t faces_size = faces.size();
+        for (size_t i = 0; i < faces_size; ++i)
+            {
+                auto [ax, ay] = project(vertices[faces[i].index1 - 1]);
+                auto [bx, by] = project(vertices[faces[i].index2 - 1]);
+                auto [cx, cy] = project(vertices[faces[i].index3 - 1]);
+                line(ax, ay, bx, by,
+                     framebuffer,
+                     red);
+                line(bx, by, cx, cy,
+                     framebuffer,
+                     red);
+            }
+    }
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
